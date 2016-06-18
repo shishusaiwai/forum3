7946853a9c2dd0807459a7224bd11f23b9e55738
shishusaiwai <shishusaiwai@vip.qq.com>
2016-06-18 15:13:59 +0800

    lesson 5:add bootstrap

--- forum/urls.py
+++ forum/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import django
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -21,5 +22,6 @@ import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', django.contrib.staticfiles.views.serve),
     url(r'^$', views.index),
 ]

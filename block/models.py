ed14af3d8f33d81f9e7d63d0d026377ceb0852bc
shishusaiwai <shishusaiwai@vip.qq.com>
2016-06-22 14:59:37 +0800

    lesson 6 part 3

--- block/models.py
+++ block/models.py
@@ -5,6 +5,8 @@ class Block(models.Model):
     name = models.CharField("板块名称", max_length=100)
     desc = models.CharField("板块描述", max_length=100)
     manager_name = models.CharField("板块管理员名称", max_length=100)
+    status = models.IntegerField("状态",
+            choices=((0, "正常"), (-1, "删除")))
 
     def __str__(self):
         return self.name
--- views.py
+++ views.py
@@ -3,5 +3,5 @@ from block.models import Block
 
 
 def index(request):
-    block_infos = Block.objects.all().order_by("-id")
+    block_infos = Block.objects.filter(status=0).order_by("-id")
     return render(request, "index.html", {"blocks": block_infos})

ae30c2f604e893cfcc2421504fd4d343377af33c
shishusaiwai <shishusaiwai@vip.qq.com>
2016-06-23 16:03:32 +0800

    lesson 7 part 2

--- article/urls.py
+++ article/urls.py
@@ -4,5 +4,5 @@ from .views import article_list
 
 
 urlpatterns = [
-    url(r'^list/', article_list),
+    url(r'^list/(?P<block_id>\d+)', article_list),
 ]
--- article/views.py
+++ article/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import render
 
 
-def article_list(request):
-    pass
+def article_list(request, block_id):
+    block_id = int(block_id)
+    return render(request, "article_list.html")

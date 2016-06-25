c06c14126c919bc48379d9a9cb2b2c43f0903a86
shishusaiwai <shishusaiwai@vip.qq.com>
2016-06-25 12:28:08 +0800

    lesson 8 part 1

--- article/urls.py
+++ article/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import article_list
+from .views import article_list, article_create
 
 
 urlpatterns = [
     url(r'^list/(?P<block_id>\d+)', article_list),
+    url(r'^create/(?P<block_id>\d+)', article_create),
 ]
--- article/views.py
+++ article/views.py
@@ -9,3 +9,9 @@ def article_list(request, block_id):
     block = Block.objects.get(id=block_id)
     articles_objs = Article.objects.filter(block=block, status=0).order_by("-id")
     return render(request, "article_list.html", {"articles": articles_objs, "b": block})
+
+
+def article_create(request, block_id):
+    block_id = int(block_id)
+    block = Block.objects.get(id=block_id)
+    return render(request, "article_create.html", {"b": block})

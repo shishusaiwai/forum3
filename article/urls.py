034f7fa09d4e17c81ba758dbbf7f3aada5dc03dc
shishusaiwai <shishusaiwai@vip.qq.com>
2016-06-29 23:10:24 +0800

    add class based-view

--- article/urls.py
+++ article/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import article_list, article_create
+from .views import article_list, ArticleCreateView, ArticleDetailView
 
 
 urlpatterns = [
     url(r'^list/(?P<block_id>\d+)', article_list),
-    url(r'^create/(?P<block_id>\d+)', article_create),
+    url(r'^create/(?P<block_id>\d+)', ArticleCreateView.as_view()),
+    url(r'^detail/(?P<pk>\d+)/$', ArticleDetailView.as_view()),
 ]
--- article/views.py
+++ article/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.views.generic import View, DetailView
 
 from block.models import Block
 from .models import Article
@@ -12,18 +13,32 @@ def article_list(request, block_id):
     return render(request, "article_list.html", {"articles": articles_objs, "b": block})
 
 
-def article_create(request, block_id):
-    block_id = int(block_id)
-    block = Block.objects.get(id=block_id)
-    if request.method == "GET":
-        return render(request, "article_create.html", {"b": block})
-    else:
+class ArticleCreateView(View):
+
+    template_name = "article_create.html"
+
+    def init_data(self, block_id):
+        self.block_id = block_id
+        self.block = Block.objects.get(id=block_id)
+
+    def get(self, request, block_id):
+        self.init_data(block_id)
+        return render(request, self.template_name, {"b": self.block})
+
+    def post(self, request, block_id):
+        self.init_data(block_id)
         form = ArticleForm(request.POST)
         if form.is_valid():
             article = form.save(commit=False)
-            article.block = block
+            article.block = self.block
             article.status = 0
             article.save()
-            return redirect("/article/list/%s" % block_id)
+            return redirect("/article/list/%s" % self.block_id)
         else:
-            return render(request, "article_create.html", {"b": block, "form": form})
+            return render(request, self.template_name, {"b": self.block, "form": form})
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+    context_object_name = 'article'

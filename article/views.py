bfb1fec9bc43dd9dd630c503bc2441bafc35beea
shishusaiwai <shishusaiwai@vip.qq.com>
2016-07-01 22:26:04 +0800

    lesson 9 part 3

--- article/views.py
+++ article/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.views.generic import View, DetailView
 
@@ -5,12 +6,30 @@ from block.models import Block
 from .models import Article
 from .forms import ArticleForm
 
+ARTICLE_CNT_1PAGE = 1
+
 
 def article_list(request, block_id):
     block_id = int(block_id)
     block = Block.objects.get(id=block_id)
-    articles_objs = Article.objects.filter(block=block, status=0).order_by("-id")
-    return render(request, "article_list.html", {"articles": articles_objs, "b": block})
+    page_no = int(request.GET.get("page_no", "1"))
+    all_articles = Article.objects.filter(block=block, status=0).order_by("-id")
+    p = Paginator(all_articles, ARTICLE_CNT_1PAGE)
+    page = p.page(page_no)
+    articles_objs = page.object_list
+
+    page_cnt = p.num_pages
+    current_no = page_no
+    page_links = [i for i in range(page_no - 5, page_no + 6)
+            if i > 0 and i <= p.num_pages]
+    previous_link = page_links[0] - 1
+    next_link = page_links[-1] + 1
+    has_previous = previous_link > 0
+    has_next = next_link <= page_cnt
+    return render(request, "article_list.html", {"articles": articles_objs, "b": block,
+            "page_cnt": page_cnt, "current_no": current_no, "page_links": page_links,
+            "previous_link": previous_link, "next_link": next_link,
+            "has_previous": has_previous, "has_next": has_next})
 
 
 class ArticleCreateView(View):
